24bb7b82612787a0cd2d603acef731b982a9df91
Glitch (anthroacoustic-happy-thought) <none>
2020-04-16 14:50:28 +0000

    🎁🐺 Checkpoint ./public/cloud.js:15007598/186

--- public/cloud.js
+++ public/cloud.js
@@ -2,14 +2,19 @@ class Cloud {
   constructor(happy, cloudImg){
     this.happyThought = happy;
     console.log(this.happyThought)
+    this.tWidth = textWidth(this.happyThought);
 
     this.width = 200;
     this.height = 100;
+    
     this.floatingOffset = random(1000);
+    
     this.pos = createVector(0,0);
     this.pos.x = -300;
     this.pos.y = random(10, height - height/8 - 50); //groundHeight
-    console.log(this.pos.y);
+    
+    
+    //console.log(this.pos.y);
     //this.vel = createVector(0, 0);
     //this.acc = createVector(0, 0);
     //this.mass = 10;
